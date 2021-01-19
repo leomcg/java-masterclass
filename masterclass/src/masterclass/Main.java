ec510ab50ac9c0caba85fb99931b95201db71e7d
Leonardo Goncalves <leomcg@gmail.com>
2021-01-19 09:58:01 -0300

    encapsulation

---
+++ masterclass/src/masterclass/Main.java
@@ -0,0 +1,17 @@
+package masterclass;
+
+public class Main {
+	public static void main(String[] args) {
+//		Player myPlayer = new Player();
+//		myPlayer.name = "Tim";
+//		myPlayer.health = 20;
+//		myPlayer.weapon = "sword";
+//		
+//		int damage = 10;
+//		myPlayer.loseHealth(damage);
+//		System.out.println("Remaining health: " + myPlayer.healthRemaining());
+//		damage = 11;
+//		myPlayer.loseHealth(damage);
+//		System.out.println("Remaining health: " + myPlayer.healthRemaining());
+	}
+}
