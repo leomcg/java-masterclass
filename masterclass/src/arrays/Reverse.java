358739c8e2eb948c85b3760a2a571f70708a6a0e
Leonardo Goncalves <leomcg@gmail.com>
2021-01-23 18:58:17 -0300

    reverse array without new array

--- masterclass/src/arrays/Reverse.java
+++ masterclass/src/arrays/Reverse.java
@@ -4,11 +4,16 @@ import java.util.Arrays;
 
 public class Reverse {
 	public static void main(String[] args) {
-		int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-		int[] reversedArray = reverseArray(myArray);
-		System.out.println(Arrays.toString(reversedArray));
+		int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+//		int[] reversedArray = reverseArray(myArray);
+//		System.out.println(Arrays.toString(reversedArray));
+		System.out.println(Arrays.toString(myArray));
+		reverse(myArray);
+		System.out.println(Arrays.toString(myArray));
 	}
 	
+	
+	//reverses the array by creating a new array
 	public static int[] reverseArray(int[] array) {
 		int[] newArray = new int[array.length];
 		int arrayLength = array.length - 1;
@@ -17,5 +22,16 @@ public class Reverse {
 			arrayLength--;
 		}
 		return newArray;
+	}
+	//reverses the array without creating a new array
+	public static void reverse(int[] array) {
+		int temp;
+		int maxIndex = array.length - 1;
+		int halfLength = array.length / 2;
+		for(int i = 0; i < halfLength; i++) {
+			temp = array[i];
+			array[i] = array[maxIndex - i];
+			array[maxIndex - i] = temp;
+		}
 	}
 }
